c0701690981e1a8366dee8f2ff686747f650b497
Md. Anwar Hossain <anwarhossain7736@gmail.com>
2025-01-30 10:18:20 +0600

    Something Changes

--- server.js
+++ server.js
@@ -1,87 +1,86 @@
-const express = require('express');
-const { createServer } = require('http');
-const { join } = require('path');
-const { Server } = require('socket.io');
+const express = require("express");
+const { createServer } = require("http");
+const { join } = require("path");
+const { Server } = require("socket.io");
 const app = express();
 const server = createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin: '*'
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
-  const users = {};
+const users = {};
 
-  io.on('connection', (socket) => {
-      socket.on('register-user', (userId) => {
-          users[userId] = socket.id;
-          socket.emit('active-users', Object.keys(users));
-          socket.broadcast.emit('active-users', Object.keys(users));
-      });
-
-      socket.on('new-user', (data) => {
-        const parsedData = JSON.parse(data);
-          socket.broadcast.emit('new-user', parsedData);
-      });
-
-      socket.on('user-updated', (data) => {
-        const parsedData = JSON.parse(data);
-          socket.broadcast.emit('user-updated', parsedData);
-      });
-
-      socket.on('user-logout', (userId) => {
-          delete users[userId];
-          socket.broadcast.emit('active-users', Object.keys(users));
-      });
-  
-      socket.on('private-message', (data) => {
-          const parsedData = JSON.parse(data);
-          io.emit('private-message', parsedData);
-      });
-
-      socket.on('message-deleted', (data) => {
-        const parsedData = JSON.parse(data);
-        io.to(users[parsedData?.receiver?._id]).emit('message-deleted', parsedData);
-      });
-
-      socket.on('add-group', (data) => {
-        const parsedData = JSON.parse(data);
-        parsedData.group_members?.map(user => {
-            io.to(users[user?.user_id]).emit('add-group', parsedData);
-        });
-      });
-      
-      socket.on('update-group', (data) => {
-        const parsedData = JSON.parse(data);
-        parsedData?.response?.map(res => {
-            io.to(users[res.user_id]).emit('update-group', {...parsedData, res});
-        });
-      });
-
-      socket.on('delete-group', (data) => {
-        const parsedData = JSON.parse(data);
-        parsedData?.groupMembers?.map(res => {
-          io.to(users[res.user_id]).emit('delete-group', parsedData);
-      });
-      });
-
-      socket.on('leave-group', (data) => {
-        const parsedData = JSON.parse(data);
-        parsedData?.groupMembers?.map(res => {
-          io.to(users[res.user_id]).emit('leave-group', parsedData);
-      });
-      });
-
-      socket.on('disconnect', () => {
-          for (const userId in users) {
-              if (users[userId] === socket.id) {
-                  delete users[userId];
-                  socket.broadcast.emit('active-users', Object.keys(users));
-                  break;
-              }
-          }
-      });
+io.on("connection", (socket) => {
+  socket.on("register-user", (userId) => {
+    users[userId] = socket.id;
+    socket.emit("active-users", Object.keys(users));
+    socket.broadcast.emit("active-users", Object.keys(users));
   });
-  
+
+  socket.on("new-user", (data) => {
+    const parsedData = JSON.parse(data);
+    socket.broadcast.emit("new-user", parsedData);
+  });
+
+  socket.on("user-updated", (data) => {
+    const parsedData = JSON.parse(data);
+    socket.broadcast.emit("user-updated", parsedData);
+  });
+
+  socket.on("user-logout", (userId) => {
+    delete users[userId];
+    socket.broadcast.emit("active-users", Object.keys(users));
+  });
+
+  socket.on("private-message", (data) => {
+    const parsedData = JSON.parse(data);
+    io.emit("private-message", parsedData);
+  });
+
+  socket.on("message-deleted", (data) => {
+    const parsedData = JSON.parse(data);
+    io.to(users[parsedData?.receiver?._id]).emit("message-deleted", parsedData);
+  });
+
+  socket.on("add-group", (data) => {
+    const parsedData = JSON.parse(data);
+    parsedData.group_members?.map((user) => {
+      io.to(users[user?.user_id]).emit("add-group", parsedData);
+    });
+  });
+
+  socket.on("update-group", (data) => {
+    const parsedData = JSON.parse(data);
+    parsedData?.response?.map((res) => {
+      io.to(users[res.user_id]).emit("update-group", { ...parsedData, res });
+    });
+  });
+
+  socket.on("delete-group", (data) => {
+    const parsedData = JSON.parse(data);
+    parsedData?.groupMembers?.map((res) => {
+      io.to(users[res.user_id]).emit("delete-group", parsedData);
+    });
+  });
+
+  socket.on("leave-group", (data) => {
+    const parsedData = JSON.parse(data);
+    parsedData?.groupMembers?.map((res) => {
+      io.to(users[res.user_id]).emit("leave-group", parsedData);
+    });
+  });
+
+  socket.on("disconnect", () => {
+    for (const userId in users) {
+      if (users[userId] === socket.id) {
+        delete users[userId];
+        socket.broadcast.emit("active-users", Object.keys(users));
+        break;
+      }
+    }
+  });
+});
 
 server.listen(3001);
